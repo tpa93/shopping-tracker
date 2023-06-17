87d6fc543deec1ff6f791898ac0c892bd35cfd71
bask_code <t.palatinus@web.de>
2023-06-17 16:21:09 +0200

    - get working save state

--- ShoppingTracker/ViewModel/DeleteTemplateViewModel.cs
+++ ShoppingTracker/ViewModel/DeleteTemplateViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 
 namespace ShoppingTracker.ViewModel
 {
     internal class DeleteTemplateViewModel
     {
+        public ObservableCollection<string> Templates { get; set; }
+        public DeleteTemplateViewModel() 
+        {
+            Templates = new ObservableCollection<string>();
+            Templates.Add("Test");
+            Templates.Add("Test");
+
+        }
     }
 }
